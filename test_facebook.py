1eebdd1dde0470e0dddcc19ee3206bb48dc295e7
Youssef Hamila <yOUSSEFHAMILA@GMAIL.COM>
2022-02-28 01:16:13 +0100

    comments

--- test_facebook.py
+++ test_facebook.py
@@ -9,9 +9,11 @@ class FacebookTest(unittest.TestCase):
         self.driver.get("https://www.facebook.com/Meta/")
     def tearDown(self):
         self.driver.close()
+    #test if posts html did not change
     def test_postExist(cls):
         html = bs(cls.driver.page_source, "lxml")
         cls.assertNotEqual(len(html.findAll("div",{"class":"du4w35lb l9j0dhe7"})),0)
+    # test if page name is Meta
     def test_pageName(cls):
         html = bs(cls.driver.page_source, "lxml")
         cls.assertEqual(getPagename(html),"Meta")
